b86f0d1d3703025c7d6ac5f6f4026006d9a59fdd
therealemg <70115362+therealemg@users.noreply.github.com>
2020-08-23 18:19:20 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+print("+ = addition | - = subtraction | * = multiplication | / = division")
+
+num1 = int(input('Enter a Number: '))
+op1 = input('Enter an operation: ')
+num2 = int(input('Enter another Number: '))
+
+if op1 == "+":
+    ans1 = num1 + num2
+    print(ans)
+
+if op1 == "-":
+    ans2 = num1 - num2
+    print(ans2)
+
+if op1 == "*":
+    ans3 = num1 * num2
+    print(ans3)
+
+if op1 == "/":
+    ans4 = num1 / num2
+    print(ans4)
